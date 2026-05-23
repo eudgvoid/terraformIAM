e8cb4b79849c6619d72afed6d3781c576e0fe281
Kacper Kalinowski <kacperkalinowski@protonmail.com>
2026-05-23 13:45:04 +0200

    Fix mixed up role and policy_attachment syntax

--- iam.tf
+++ iam.tf
@@ -1,3 +1,15 @@
+data "aws_iam_policy_document" "ec2_trust_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
 # iam_group
 resource "aws_iam_group" "this_group" {
   name = "cmtr-6pajwelx-iam-group"
@@ -19,28 +31,15 @@ resource "aws_iam_policy" "this_policy" {
 
 }
 
-data "aws_iam_policy_document" "ec2_trust_policy" {
-  statement {
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "this_role" {
-  name = "cmtr-6pajwelx-iam-role"
-
+resource "aws_iam_role" "this_role" {
+  name                = "cmtr-6pajwelx-iam-role"
   managed_policy_arns = [aws_iam_policy.this_policy.arn]
   assume_role_policy  = data.aws_iam_policy_document.ec2_trust_policy.json
 }
 
-resource "aws_iam_policy_attachment" "this_attachment" {
-  name  = "cmtr-6pajwelx-iam-attachment"
-  roles = [aws_iam_role.this_role.name]
+resource "aws_iam_role_policy_attachment" "this_attachment" {
+  #name  = "cmtr-6pajwelx-iam-attachment"
+  role = aws_iam_role.this_role.name
   #groups     = [aws_iam_group.group.name]
   policy_arn = aws_iam_policy.this_policy.arn
 }
